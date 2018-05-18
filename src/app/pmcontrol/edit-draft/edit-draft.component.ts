2af380e1935e350412722581ed9666cd4f14cc11
ford <ftn2539@gmail.com>
2018-05-19 01:01:21 +0700

    Pm edit Draft

---
+++ src/app/pmcontrol/edit-draft/edit-draft.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit,Inject } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-draft',
+  templateUrl: './edit-draft.component.html',
+  styleUrls: ['./edit-draft.component.css']
+})
+export class EditDraftComponent implements OnInit {
+
+  constructor(private _formBuilder: FormBuilder) { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required]
+    });
+  }
+
+}
